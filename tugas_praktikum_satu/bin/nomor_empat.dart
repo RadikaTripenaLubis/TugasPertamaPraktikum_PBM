f15c3f1aa54fa46ca563fb530ce2758394fa9561
RadikaTripenaLubis <114640423+RadikaTripenaLubis@users.noreply.github.com>
2022-10-12 19:07:04 +0700

    Update nomor_empat.dart

--- tugas_praktikum_satu/bin/nomor_empat.dart
+++ tugas_praktikum_satu/bin/nomor_empat.dart
@@ -5,7 +5,7 @@ void main(List<String> args) {
   double ipk = double.parse(stdin.readLineSync());
 
   stdout.write('Jumlah MatKul yang diulang: ');
-  double remed = double.parse(stdin.readLineSync());
+  int remed = int.parse(stdin.readLineSync());
 
   if (ipk >= 3.5 && remed == 0) {
     print('Selamat Anda CUMLAUDE');
