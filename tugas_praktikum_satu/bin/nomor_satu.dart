f1b709753287469fa6307e11882fc8808f5798cc
RadikaTripenaLubis <114640423+RadikaTripenaLubis@users.noreply.github.com>
2022-10-12 19:06:18 +0700

    Update nomor_satu.dart

--- tugas_praktikum_satu/bin/nomor_satu.dart
+++ tugas_praktikum_satu/bin/nomor_satu.dart
@@ -5,9 +5,11 @@ void main() {
   nama = 'Radika Tripena Lubis';
   kls = '5E';
   int npm = 2010631170110;
+  double ipk = 3.75;
   var prodi = 'INFORMATIKA';
   bool lulus = false;
 
   print("Nama  : $nama. \nKelas : $kls. \nNPM   : $npm.\nProgram Studi $prodi");
+  print("IPK saya $ipk");
   print("Sudah lulus? $lulus\n");
 }
